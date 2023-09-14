70899ecdcabeafb22877f31bc51b51628dfa874f
João Pedro Martins Filho <jpmartinsfilho1@gmail.com>
2023-09-14 01:21:46 -0300

    1

--- Front/botoes.js
+++ Front/botoes.js
@@ -1,29 +1,35 @@
 //Altera de pagina cliente
 function cadastroCliente() {
-    console.log("click cliente");
-    window.location.href = "cadastroCliente.html";
-  }
-  
-  //Altera de pagina cliente
-  function cadastroProduto() {
-    console.log("click validade");
-    window.location.href = "cadastroProduto.html";
-  }
-  
-  //Altera de pagina busca de produtos
-  function buscaProduto() {
-    console.log("click sacola");
-    window.location.href = "buscaProduto.html";
-  }
-  
-  //Altera de pagina busca de cliente
-  function buscaCliente() {
-    console.log("click sacola");
-    window.location.href = "buscaCliente.html";
-  }
-  
-  //Altera de pagina endereço
-  function endereco() {
-    console.log("click endereco");
-    window.location.href = "endereco.html";
-  }
+  console.log("click cliente");
+  window.location.href = "cadastroCliente.html";
+}
+
+//Altera de pagina cliente
+function cadastroProduto() {
+  console.log("click validade");
+  window.location.href = "cadastroProduto.html";
+}
+
+//Altera de pagina busca de produtos
+function buscaProduto() {
+  console.log("click sacola");
+  window.location.href = "buscaProduto.html";
+}
+
+//Altera de pagina busca de cliente
+function buscaCliente() {
+  console.log("click sacola");
+  window.location.href = "buscaCliente.html";
+}
+
+//Altera de pagina endereço
+function endereco() {
+  console.log("click endereco");
+  window.location.href = "endereco.html";
+}
+
+//Altera de pagina endereço
+function index() {
+  console.log("click index");
+  window.location.href = "index.html";
+}
