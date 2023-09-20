fbe6f487030b0d1937cd6a55f165b37ed6085448
João Pedro Martins Filho <jpmartinsfilho1@gmail.com>
2023-09-20 17:57:29 -0300

    1

--- Front/js/requisicao.js
+++ Front/js/requisicao.js
@@ -46,3 +46,90 @@ function getList(api) {
     console.error("Error:", error);
   }
 }
+/*
+//Consulta para id
+function buscaGet(porta, api, parametro, item) {
+  let url =
+    "http://127.0.0.1:" +
+    porta +
+    "/" +
+    api +
+    parametro +
+    "?" +
+    parametro +
+    "=" +
+    item;
+
+  //get do objeto
+  console.log("get");
+  console.log(parametro);
+  try {
+    fetch(url, {
+      method: "get",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.id != null) {
+        } else {
+          alert("Produto não encontrado");
+        }
+      })
+      .catch((error) => {
+        if (error instanceof TypeError) {
+          TratamentoTypeError(error);
+        } else if (error.message === "Failed to fetch") {
+          TratamentoFetchError();
+        } else {
+          // Relance o erro se não for um TypeError ou um erro de conexão
+          throw error;
+        }
+      });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}*/
+/*
+//Consulta para varios
+function buscaGetmais(porta, api, parametro, item) {
+  let url =
+    "http://127.0.0.1:" +
+    porta +
+    "/" +
+    api +
+    parametro +
+    "?" +
+    parametro +
+    "=" +
+    item;
+
+  //get do objeto
+  console.log("get");
+  console.log(url);
+  try {
+    fetch(url, {
+      method: "get",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.itens);
+        if (data.itens != 0) {
+        } else {
+          alert("Prodduto não encontrado");
+        }
+      })
+      .catch((error) => {
+        if (error instanceof TypeError) {
+          TratamentoTypeError(error);
+        } else if (error.message === "Failed to fetch") {
+          TratamentoFetchError();
+        } else {
+          // Relance o erro se não for um TypeError ou um erro de conexão
+          throw error;
+        }
+      });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+*/
