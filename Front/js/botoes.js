8ef2363e9eaa89f3d45a667a01b6e08612d62035
João Pedro Martins Filho <jpmartinsfilho1@gmail.com>
2023-09-22 17:01:38 -0300

    1

--- Front/js/botoes.js
+++ Front/js/botoes.js
@@ -1,40 +1,40 @@
-//Altera de pagina cliente
+//Carrega página cliente
 function cadastroCliente() {
   console.log("click cliente");
   window.location.href = "../html/cadastroCliente.html";
 }
 
-//Altera de pagina cliente
+//Carrega página cliente
 function cadastroProduto() {
   console.log("click validade");
   window.location.href = "../html/cadastroProduto.html";
 }
 
-//Altera de pagina endereço
+//Carrega página endereço
 function cadastroCompra() {
   console.log("click index");
   window.location.href = "../html/cadastroCompra.html";
 }
 
-//Altera de pagina consulta de produtos
+//Carrega página consulta de produtos
 function consultaProduto() {
   console.log("click sacola");
   window.location.href = "../html/consultaProduto.html";
 }
 
-//Altera de pagina consulta de cliente
+//Carrega página consulta de cliente
 function consultaCliente() {
   console.log("click sacola");
   window.location.href = "../html/consultaCliente.html";
 }
 
-//Altera de pagina endereço
+//Carrega página endereço
 function consultaCompra() {
   console.log("click index");
   window.location.href = "../html/consultaCompra.html";
 }
 
-//Altera de pagina endereço
+//Carrega página endereço
 function index() {
   console.log("click index");
   window.location.href = "../html/index.html";
