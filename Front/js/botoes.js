dd2bf1e424f5a3ebd208104f78dbf8a06b29f2a2
João Pedro Martins Filho <jpmartinsfilho1@gmail.com>
2023-09-24 02:18:30 -0300

    1

--- Front/js/botoes.js
+++ Front/js/botoes.js
@@ -1,43 +1,44 @@
-//Carrega página cliente
+//Script do código dos botões de carregamentodas páginas
+//Carrega página cadastro de cliente
 function cadastroCliente() {
   setTimeout(function () {
     isClicked = false;
   }, 5000); // Atraso de 5 segundo
-  console.log("click cliente");
+  console.log("click cadastroCliente");
   window.location.href = "../html/cadastroCliente.html";
 }
 
-//Carrega página cliente
+//Carrega página cadastro de produto
 function cadastroProduto() {
-  console.log("click validade");
+  console.log("click cadastroProduto");
   window.location.href = "../html/cadastroProduto.html";
 }
 
-//Carrega página endereço
+//Carrega página cadastro de compra
 function cadastroCompra() {
-  console.log("click index");
+  console.log("click cadastroCompra");
   window.location.href = "../html/cadastroCompra.html";
 }
 
 //Carrega página consulta de produtos
 function consultaProduto() {
-  console.log("click sacola");
+  console.log("click consultaProduto");
   window.location.href = "../html/consultaProduto.html";
 }
 
 //Carrega página consulta de cliente
 function consultaCliente() {
-  console.log("click sacola");
+  console.log("click consultaCliente");
   window.location.href = "../html/consultaCliente.html";
 }
 
-//Carrega página endereço
+//Carrega página consulta de compra
 function consultaCompra() {
-  console.log("click index");
+  console.log("click consultaCompra");
   window.location.href = "../html/consultaCompra.html";
 }
 
-//Carrega página endereço
+//Carrega página index
 function index() {
   console.log("click index");
   window.location.href = "../html/index.html";
